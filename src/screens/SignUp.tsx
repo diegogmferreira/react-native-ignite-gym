9246ccd8e55cf041bec2be23c4a06c948d46fd09
Diego Galvao <di.galvao89@gmail.com>
2024-05-13 21:37:00 -0300

    Finishes SignIn screen and adds SignUp

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -5,7 +5,7 @@ import LogoSvg from '@assets/logo.svg';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
-export function SignIn() {
+export function SignUp() {
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
       <VStack flex={1} bg={"gray.700"} px={10}>
@@ -26,9 +26,12 @@ export function SignIn() {
 
         <Center>
           <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-            Acesse sua conta
+            Crie sua conta
           </Heading>
 
+          <Input
+            placeholder='Nome'
+          />
           <Input
             placeholder='E-mail'
             keyboardType='email-address'
@@ -40,20 +43,15 @@ export function SignIn() {
           />
 
           <Button
-            title='Acessar'
+            title='Criar e acessar'
           />
         </Center>
 
-        <Center mt={24}>
-          <Text color={"gray.100"} fontSize="sm" mb={3} fontFamily="body">
-            Ainda não tem acesso?
-          </Text>
-          <Button
-            title='Criar conta'
-            variant={"outline"}
-          />
-        </Center>
-
+        <Button
+          title='Voltar para o login'
+          variant={"outline"}
+          mt={24}
+        />
       </VStack>
     </ScrollView>
   );
