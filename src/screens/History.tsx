6a23e202d63a5e752e8b08ecfda2126d9e952aa1
Diego Galvao <di.galvao89@gmail.com>
2024-05-18 16:36:38 -0300

    Adds History screen content

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,47 @@
-import { Center, Text } from "native-base";
+import { HistoryCard } from "@components/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Center, Heading, Text, VStack, SectionList } from "native-base";
+import { useState } from "react";
 
 export function History() {
-  return(
-    <Center flex={1}>
-      <Text>History</Text>
-    </Center>
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.23',
+      data: ['Puxada frontal', 'Remada unilateral']
+    },
+    {
+      title: '27.08.23',
+      data: ['Puxada frontal']
+    }
+  ])
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de Exercícios" />
+
+      <SectionList
+        px={8}
+        sections={exercises}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({ section }) => (
+          <Heading color='gray.200' fontSize='md' mt={10} mb={3}>
+            {section.title}
+            </Heading>
+        )}
+        contentContainerStyle={exercises.length === 0 && {
+          flex: 1,
+          justifyContent: 'center',
+        } }
+        ListEmptyComponent={() => (
+          <Text color='gray.100' textAlign='center'>
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos treinar hoje?
+          </Text>
+        )}
+      />
+      <HistoryCard />
+    </VStack>
   )
 }
